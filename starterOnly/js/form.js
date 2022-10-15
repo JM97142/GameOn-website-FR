5f99f6e7b6ddff27453795074a12f68c4ea6f340
JM97142 <jeanmichel.taret@gmail.com>
2022-10-16 01:30:54 +0200

    Correction attributs for et id/Ajout fichir form et DOM

---
+++ starterOnly/js/form.js
@@ -0,0 +1,12 @@
+
+// DOM Elements
+const form = document.querySelectorAll("#form");
+const firstInput = document.querySelector("#first");
+const lastInput = document.querySelector("#last");
+const emailInput = document.querySelector("#email");
+const birthdateInput = document.querySelector("#birthdate");
+const quantityInput = document.querySelector("#quantity");
+const locationChoice = document.querySelector(".location-choice");
+const locationInputs = Array.from(document.querySelectorAll(".location-input"));
+const conditionsInput = document.querySelector(".conditions-input");
+const newsInput = document.querySelector(".news-input");
