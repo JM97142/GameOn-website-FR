d66afdb4a988841bbb404f66d466c8b26cecd92d
JM97142 <jeanmichel.taret@gmail.com>
2022-10-16 01:04:49 +0200

    Issue 1: fermer la modal

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,23 +1,27 @@
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += " responsive";
+      x.className += " responsive";
   } else {
-    x.className = "topnav";
+      x.className = "topnav";
   }
 }
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// close modal event
+modalCloseBtn.addEventListener('click', closeModal);
+
+// launch/close modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
+function closeModal() {
+  modalbg.style.display = "none";
+}
